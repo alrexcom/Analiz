fe54b8dd1d0a9b1f93ba71f41d6e85df54c27577
Alrexcom <mpstmprt@mail.ru>
2024-04-22 20:16:51 +0300

    Сделал 2 отчёта

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # -*- coding: cp1251 -*-
 #  from base64 import b16decode
 # from ctypes.wintypes import INT
+from typing import Callable, Any
+
 import pandas as pd
 from os import path
 import tkinter as tk
@@ -18,16 +20,17 @@ text.tag_config('title', justify=LEFT,
                 font=("Verdana", 24, 'bold'))
 
 
-
-
-def get_data(file_name, one_hour_fte=164, rprt=1):
-    global fr
+def get_data(file_name, rprt, one_hour_fte=164):
     # Данные по ресурсным планам и списанию трудозатрат сотрудников за период
+    num_row_header: Callable[[Any], int] = lambda rprt: 0 if rprt == 1 else 1  # if rprt == 2 else 1
+
+    df = pd.read_excel(file_name, header=num_row_header(rprt))
     if rprt == 1:
-        df = pd.read_excel(file_name, header=0)
-        fr = df[df['Менеджер проекта'] == 'Тапехин Алексей Александрович'][['Проект', 'План, FTE', 'Пользователь',
-                                                                            'Фактические трудозатраты (час.) (Сумма)',
-                                                                            'Кол-во штатных единиц']]
+        headers = ['Проект', 'План, FTE', 'Пользователь', 'Фактические трудозатраты (час.) (Сумма)',
+                   'Кол-во штатных единиц']
+
+        fr = df[headers].loc[df['Менеджер проекта'] == 'Тапехин Алексей Александрович']
+
         fr['Факт, FTE'] = round(fr['Фактические трудозатраты (час.) (Сумма)'] / one_hour_fte, 2)
         fr['Часы план'] = one_hour_fte * fr['План, FTE']
         fr['Остаток часов'] = fr['Часы план'] - fr['Фактические трудозатраты (час.) (Сумма)']
@@ -35,44 +38,42 @@ def get_data(file_name, one_hour_fte=164, rprt=1):
                          'Факт, FTE', 'Остаток часов'])['Фактические трудозатраты (час.) (Сумма)'].sum()
     # Контроль заполнения факта за период
     elif rprt in (2, 3):
-        df = pd.read_excel(file_name, header=1)
+        # df = pd.read_excel(file_name, header=1)
         fr = df[(df['Проект'] == 'Т0133-КИС "Производственный учет и отчетность"') |
                 (df['Проект'] == 'С0134-КИС "Производственный учет и отчетность"')][
             ['Проект', 'ФИО', 'Дата', 'Трудозатрады за день']]
         if rprt == 3:
             fr = fr.sort_values(['Проект', 'ФИО', 'Дата'])
         else:
-            fr = round(fr.groupby(['Проект', 'ФИО'])['Трудозатрады за день'].sum(), 2)
+            # fr = round(fr.groupby(['Проект', 'ФИО'])['Трудозатрады за день'].sum(), 2)
+            fr = fr.groupby(['Проект', 'ФИО']).agg({'Дата': 'max', 'Трудозатрады за день': 'sum'})
+
+    if export_excel.get():
+        save_file = path.dirname(file_name) + '/output.xlsx'
+        label.config(text=f"Файл сохранился в {save_file}")
+        fr.to_excel(save_file, index=True)
     return fr
 
 
-def print_text(filename):
-    return f'{filename}\n \n \n {fr}'
+def print_text(filename, rprt):
+    return f'{filename}\n \n \n {get_data(filename, rprt=rprt)}'
 
 
 # Создаем функцию, которая будет вызываться при нажатии кнопки
 def on_button_click():
-
     filename = filedialog.askopenfilename()
     label.config(text="")
     text.delete(1.0, END)
-
+    msg = ""
     if check_box1.get():
-        fr = get_data(filename, rprt=1)
+        msg = print_text(filename, rprt=1)
     if check_box2.get():
         if export_excel.get():
-            fr = get_data(filename, rprt=3)
-            # fr = un.get_data(filename, rprt=2)
+            msg = print_text(filename, rprt=3)
         else:
-            fr = get_data(filename, rprt=2)
-        # fr = un.get_data(filename, rprt=lambda rp: 3 if export_excel.get() else 2)
-    # arg = [filename, fr]
-    text.insert(5.0, print_text(filename))
+            msg = print_text(filename, rprt=2)
 
-    if export_excel.get():
-        save_file = path.dirname(filename) + '/output.xlsx'
-        label.config(text=f"Файл сохранился в {save_file}")
-        fr.to_excel(save_file, index=True)
+    text.insert(5.0, msg)
 
 
 # Создаем виджеты (например, кнопку, текстовое поле и метку)
